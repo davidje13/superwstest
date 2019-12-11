e659f8e619777219a44bef6503da20319ba6c07c
David Evans <davidje13@users.noreply.github.com>
2019-12-11 00:24:36 +0000

    Disable noisy lint rule

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -13,6 +13,7 @@ module.exports = {
         rules: {
           'arrow-parens': ['error', 'always'],
           'operator-linebreak': ['error', 'after'],
+          'jest/expect-expect': ['off'],
         },
       },
     }),
