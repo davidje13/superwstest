8b2af2edaeecb8f6ef61185f53fdfdc4c9543176
David Evans <davidje13@users.noreply.github.com>
2024-06-29 10:22:04 +0100

    Remove workaround for Node 14

--- test/run-package.sh
+++ test/run-package.sh
@@ -16,7 +16,7 @@ cd "$BASE_DIR/test/package";
 rm -rf node_modules || true;
 npm install --audit=false;
 rm superwstest.tgz || true;
-npm test --ignore-scripts=false; # ignore-scripts is over-zealous on Node 14
+npm test;
 cd - >/dev/null;
 
 echo;
